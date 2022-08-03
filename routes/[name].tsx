3c1de20b11013382b7ccb472ee33a17a24e490f0
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2022-08-03 14:46:13 +0200

    *** FIX *** Bind the server to 0.0.0.0

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,12 +1,18 @@
 /** @jsx h */
 import { h } from "preact";
 import { PageProps } from "$fresh/server.ts";
+import { platform } from "https://deno.land/std@0.150.0/node/os.ts";
 
 export default function Greet(props: PageProps) {
-  function capitalizeFirstLetter(a:string) {
+  function capitalizeFirstLetter(a: string) {
     return a.charAt(0).toUpperCase() + a.slice(1);
   }
 
-  return <div>Hello { capitalizeFirstLetter(props.params.name) }!<br/>The server time is: { new Date().toDateString() } </div>;
+  return (
+    <div>
+      Hello {capitalizeFirstLetter(props.params.name)}!<br />
+      The server time is: {new Date().toDateString()}<br/>
+      The server platform is: {platform() }
+    </div>
+  );
 }
- 
